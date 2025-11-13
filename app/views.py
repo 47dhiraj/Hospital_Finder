1b078b3c1f59dad8f6a23fbda8f4a105820a6957
Dhiraj Kafle <dhirajkafle19@gmail.com>
2025-11-13 20:57:04 +0545

    Email settings working fine

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
+from django.conf import settings
+
+from .models import *
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout as auth_logout
@@ -9,11 +13,12 @@ from django.http import HttpResponseRedirect
 from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
+
+
+
+from django.core.mail import send_mail
 from django.core.mail import EmailMessage
-from django.conf import settings
 
-
-from .models import *
 
 import sweetify
 
@@ -38,6 +43,32 @@ from datetime import timedelta
 
 def home(request):
     
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        contact_detail = "\n Name: " + name + "\n Email: " + email + "\n\n Message: " + message
+
+        if not request.user.is_authenticated:
+            subject = "A Visitor's Comment"
+        else:
+            subject = str(request.user) + "'s Comment"
+
+
+        send_mail(
+            subject=subject,
+            message=contact_detail,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+ 
+ 
+        messages.success(request, ' Thank you for getting in touch! ')
+
+
     return render(request, 'app/index.html')
 
 
